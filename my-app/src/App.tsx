676161d200cfbac3643588067f5f7671a81035e3
Mako Matsuno <mako_matsuno@amewatafuwari.local>
2020-05-23 23:27:37 +0900

    refactor: Rewritten the list of prefectures to make it easier to understand.
    useEffect内にあったfetchAPIの処理を外に出した。
    fetchAPIの処理にstatus code判定とエラー時の処理を追加。
    各種不適切な命名を修正。
    型注釈やuseStateの引数を適切なものにした。

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { PrefectureData } from './api/prefecture'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <PrefectureData />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- my-app/src/api/prefecture.tsx
+++ my-app/src/api/prefecture.tsx
@@ -1,24 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-function PrefectureData() {
-    const [state, setPrefecture] = useState(0);
-
-    useEffect( () => {
-        (async () => {
-            const prefecture = await fetch(	'https://opendata.resas-portal.go.jp',
-            {
-                headers: { apiKey: process.env.API_KEY}
-            }
-            )
-            const res = await prefecture.json();
-        })();},[]);
-
-    return (
-        <div>
-            <p>You clicked {state} times</p>
-            <button onClick={() => setPrefecture(state)}>
-                Click me
-            </button>
-        </div>
-    );
+type Prefecture = {
+    prefCode: number,
+    prefName: string,
 }
+
+const [statePrefecture, setPrefectureList ] = useState<Prefecture>();
+
+const fetchData = async () => {
+    const url = 'https://opendata.resas-portal.go.jp/api/v1/prefectures'
+    const apiKey = process.env.API_KEY
+
+    const response = await fetch(url,{headers:{apiKey}});
+    if (!response.ok) {
+        throw new Error(`API Error: ${response.status} ${response.statusText}`);
+    }
+    setPrefectureList((await response.json()).result)
+};
+
+
+const PrefectureListResourceView = () => {
+    useEffect(() => {
+    　　 (async () => {
+            await fetchData()
+        })();
+    }, []);
+    return statePrefecture;
+};
+
+export { PrefectureListResourceView }
